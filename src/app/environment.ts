31dab9ddf7030de800aaa32632519528066c028a
AlexVare <alex_varela_13@hotmail.com>
2024-05-14 10:13:37 +0200

    Cambiar rutas de api

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  URLServer: "https://viveiro-back20230712113858.azurewebsites.net/api/",
-  API_KEY_STRIPE : "",
+  URLServer: "https://lotospaindwapi-3gojg2v3ha-od.a.run.app/api/",
+  API_KEY_STRIPE: "",
 };
